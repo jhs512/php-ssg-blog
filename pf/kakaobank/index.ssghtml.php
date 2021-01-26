cd6b6f4285edb911ca5c06935def6427c38a6c19
jhs512 <jangka512@gmail.com>
2021-01-26 15:38:55 +0900

    내부로직개선

---
+++ pf/kakaobank/index.ssghtml.php
@@ -0,0 +1,7 @@
+<?php
+require_once "head.php";
+?>
+<h1>안녕</h1>
+<?php
+require_once "foot.php";
+?>
---
+++ pf/kakaobank/sub1.ssghtml.php
@@ -0,0 +1,7 @@
+<?php
+require_once "head.php";
+?>
+<h1>ㅋㅋ</h1>
+<?php
+require_once "foot.php";
+?>
