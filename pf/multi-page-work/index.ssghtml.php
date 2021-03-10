a82b0b74fe842cd16855855641e0d9ea93fb6e4c
jhs512 <jangka512@gmail.com>
2021-03-10 18:45:24 +0900

    같은 패턴의 디자인을 무식하게 복붙한 예

--- pf/multi-page-work/index.ssghtml.php
+++ pf/multi-page-work/index.ssghtml.php
@@ -5,6 +5,17 @@ $pageCode = "home";
 require_once "head.php";
 ?>
 
+<section class="section-bn-1 con-min-width">
+  <div class="con"><img class="block w-full" src="https://picsum.photos/id/237/1200/500" alt=""></div>
+</section>
+
+<section class="section-1 section con-min-width">
+  <div class="con">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione, illum? Perferendis eveniet autem rerum, quisquam libero unde animi ducimus exercitationem, odio facilis et dolorem. Fuga eum deleniti debitis doloremque veritatis.
+    Doloribus, odio quas commodi porro id dolor alias aperiam dolorum harum magnam assumenda eius autem accusamus voluptatibus accusantium corrupti aliquid ipsa sunt, architecto voluptas. Error assumenda accusamus totam magni sunt!
+  </div>
+</section>
+
 <?php
 require_once "foot.php";
 ?>
