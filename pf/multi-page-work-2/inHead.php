556579cc0d9d1c7612921bdb1e4821660f143496
jhs512 <jangka512@gmail.com>
2021-03-12 17:53:53 +0900

    head 엘리먼트안의 내용관련 중복 제거

---
+++ pf/multi-page-work-2/inHead.php
@@ -0,0 +1,16 @@
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title><?=$pageTitle?> - G화학</title>
+
+<!-- 테일윈드 불러오기 -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.3/tailwind.min.css">
+
+<!-- 폰트어썸 불러오기 -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+
+<!-- 제이쿼리 불러오기 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<link rel="stylesheet" href="common.css">
+<script defer src="common.js"></script>
