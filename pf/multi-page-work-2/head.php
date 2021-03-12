24df246b4f2986157f5c12ce98933bf14eef3e00
jhs512 <jangka512@gmail.com>
2021-03-12 18:15:35 +0900

    검색박스가 메인에만 나오도록

--- pf/multi-page-work-2/head.php
+++ pf/multi-page-work-2/head.php
@@ -42,10 +42,9 @@
 
 <body>
   <div class="site-wrap">
-    <header class="top-bar con-min-width">
-      <a href="index.ssghtml.php">홈</a>
-      <a href="sub1.ssghtml.php">서브1</a>
-      <a href="sub2.ssghtml.php">서브2</a>
-    </header>
+    <?php
+    $headCode = "head";
+    ?>
+    <?php require_once "part.topBar.php"; ?>
 
     <main>
---
+++ pf/multi-page-work-2/part.topBar.php
@@ -0,0 +1,9 @@
+<header class="top-bar con-min-width">
+  <a href="index.ssghtml.php">홈</a>
+  <a href="sub1.ssghtml.php">서브1</a>
+  <a href="sub2.ssghtml.php">서브2</a>
+  <a href="sub3.ssghtml.php">서브3</a>
+  <?php if ( $headCode == "head" ) { ?>
+    <input type="text" placeholder="검색어를 입력해주세요.">
+  <?php } ?>
+</header>
