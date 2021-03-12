efefe24ce0d16f740a63137b7026540e861c09bc
jhs512 <jangka512@gmail.com>
2021-03-12 18:16:53 +0900

    홈과 서브2페이지에만 검색박스 노출

--- pf/multi-page-work-2/part.topBar.php
+++ pf/multi-page-work-2/part.topBar.php
@@ -3,7 +3,7 @@
   <a href="sub1.ssghtml.php">서브1</a>
   <a href="sub2.ssghtml.php">서브2</a>
   <a href="sub3.ssghtml.php">서브3</a>
-  <?php if ( $headCode == "head" ) { ?>
+  <?php if ( $pageCode == "home" || $pageCode == "sub2" ) { ?>
     <input type="text" placeholder="검색어를 입력해주세요.">
   <?php } ?>
 </header>
