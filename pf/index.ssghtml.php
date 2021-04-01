386d04c0f790433ad8eec13b4ee8efe1610dba88
jhs512 <jangka512@gmail.com>
2021-04-01 18:49:27 +0900

    .

---
+++ pf/index.ssghtml.php
@@ -0,0 +1,12 @@
+<?php
+$pageCode = 'index';
+$pageTitle = "홈";
+require_once "head.php";
+?>
+<link rel="stylesheet" href="index.css">
+<script src="index.js" defer></script>
+<h1>메인></h1>
+
+<?php
+require_once "foot.php";
+?>
